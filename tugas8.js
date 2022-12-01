32487b730d093b052d8aaa278c2641d5f26b1ebc
NadyaTambunan <nadiaputri99tambunan@gmail.com>
2022-12-01 14:33:28 +0700

    tugas8.js

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+function tugas8(){
+    var laptop = {
+        merk: "Asus",
+        harga: 10000000,
+        warna: "silver",
+    }
+
+    laptop.merk = "Lenovo"
+
+    console.log(laptop.merk)
+    console.log(laptop.harga)
+    console.log(laptop.warna)
+}
+
+tugas8()
